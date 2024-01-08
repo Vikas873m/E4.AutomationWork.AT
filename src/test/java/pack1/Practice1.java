d4509beb43be33ef98f69f4d6c9e4e34bcf04429
VikasAT <VikasAT@DESKTOP-IOTR1HB>
2024-01-08 10:51:55 +0530

    commit and push

---
+++ src/test/java/pack1/Practice1.java
@@ -0,0 +1,36 @@
+package pack1;
+
+public class Practice1 {
+	static void over() {
+		System.out.println("Perform Method Overloading For Static Method Without Argument");
+		
+	}
+	static void over(int a, float b) {
+		System.out.println(a+" "+b);
+	}
+	static void over(float b, int a) {
+		System.out.println(b+ " "+a);
+	}
+	static void over( int a, float b, char c, String s, double d) {
+		System.out.println(a+ " "+b+" "+c+" "+s+" "+d);
+	}
+	static void over(int a, int i, long f, boolean g, char h, int j ) {
+		System.out.println(a+ " "+i+" "+f+" "+g+" "+ h +" "+ j);
+	}
+	 void over(long l, float b) {
+		System.out.println(l+ " "+b); // Non Static Method for overloading
+	 }
+	public static void main(String[]args) {
+		
+	
+	System.out.println("Resut for method overloading is given below");
+	over();//Printing OverLoading Without Argument.
+	over(10,11.12f);//Printing OverLoading With Argument.
+	over(12.64f,14);//3rd.
+	over(10,12.24f,'D', "overloading value",21.20);//4th.
+	over(10,20,35246,true,'G',40);//5th
+	Practice1 a1= new Practice1();// Creating an object of non static method for MethodOverloading.
+	Practice1.over(693656,20.20f);
+	}
+	}
+	
